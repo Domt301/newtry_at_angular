005f1315ae1685c50df0b841ec0124c90ad3802c
Leonardo Trimarchi <dom@Leonardos-Mac-mini.local>
2017-09-19 23:26:06 -0400

    added search component and data binding i think?

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule} from '@angular/forms';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import ApplicationComponent from './components/application/application';
-import CarouselComponent from './components/carousel/carousel';
+import CarouselComponent from "./components/carousel/carousel";
 import FooterComponent from "./components/footer/footer";
 import NavbarComponent from "./components/navbar/navbar";
 import ProductItemComponent from "./components/product-item/product-item";
@@ -12,26 +13,26 @@ import StarsComponent from "./components/stars/stars";
 import {ProductService} from "./services/product-service";
 import HomeComponent from "./components/home/home";
 import ProductDetailComponent from "./components/product-detail/product-detail";
-
+import {FilterPipe} from "./components/pipes/filter-pipe";
 
 @NgModule({
-    imports:      [ BrowserModule, RouterModule.forRoot([
-        {path:'', component:HomeComponent},{
-            path: 'products/:productId', component: ProductDetailComponent
-        }
-
+    imports:      [ BrowserModule, ReactiveFormsModule,
+                    RouterModule.forRoot([
+                        {path: '',                    component: HomeComponent},
+                        {path: 'products/:productId', component: ProductDetailComponent}
     ]) ],
     declarations: [ ApplicationComponent,
-        CarouselComponent,
-        FooterComponent,
-        NavbarComponent,HomeComponent,ProductDetailComponent,
-        ProductItemComponent,
-        SearchComponent,
-        StarsComponent],
-
-    providers:    [ProductService,{
-        provide: LocationStrategy, useClass: HashLocationStrategy
-    }],
+                    CarouselComponent,
+                    FooterComponent,
+                    NavbarComponent,
+                    HomeComponent,
+                    ProductDetailComponent,
+                    ProductItemComponent,
+                    SearchComponent,
+                    StarsComponent,
+                    FilterPipe],
+    providers:    [ProductService,
+                   {provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap:    [ ApplicationComponent ]
 })
 export class AppModule { }
