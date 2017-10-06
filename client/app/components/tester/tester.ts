248337526718c513afa7760b5ebae5b1b3c0fa3e
Domt301 <leonardod.trimarchi@gmail.com>
2017-10-06 11:05:17 -0400

    added tester component

---
+++ client/app/components/tester/tester.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router'
+
+@Component({
+    selector: 'tester-tester',
+    template: `<h1>Hi Im a tester component!</h1>`
+})
+
+export default class TesterComponent{}
